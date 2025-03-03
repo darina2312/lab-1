0b00145933a2326314e40de205b3078bae674bfa
darina2312 <darina.dzhekieva@gmail.com>
2025-03-03 18:13:28 +0300

    division added

---
+++ build/CMakeFiles/lab-1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab-1.dir/src/main.cpp.o"
+  "CMakeFiles/lab-1.dir/src/main.cpp.o.d"
+  "CMakeFiles/lab-1.dir/src/utils.cpp.o"
+  "CMakeFiles/lab-1.dir/src/utils.cpp.o.d"
+  "lab-1"
+  "lab-1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/utils.cpp
+++ src/utils.cpp
@@ -9,3 +9,6 @@ int subtract(int a, int b) {
 int multiply(int a, int b) {
     return a * b;
 }
+int divide(int a, int b) {
+    return a / b;
+}
